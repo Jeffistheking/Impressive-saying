0e136ca387866f8756808548c40bbb9191a2671f
Jeffistheking <90879280+Jeffistheking@users.noreply.github.com>
2023-09-16 09:06:36 +0900

    수정 2

--- myScript.js
+++ myScript.js
@@ -1,17 +1,33 @@
-var tabButtons=document.querySelectorAll(".tabContainer .buttonContainer button");
-var tabPanels=document.querySelectorAll(".tabContainer  .tabPanel");
+// Automatic Slideshow - change image every 4 seconds
+var myIndex = 0;
+carousel();
 
-function showPanel(panelIndex,colorCode) {
-    tabButtons.forEach(function(node){
-        node.style.backgroundColor="";
-        node.style.color="";
-    });
-    tabButtons[panelIndex].style.backgroundColor=colorCode;
-    tabButtons[panelIndex].style.color="white";
-    tabPanels.forEach(function(node){
-        node.style.display="none";
-    });
-    tabPanels[panelIndex].style.display="block";
-    tabPanels[panelIndex].style.backgroundColor=colorCode;
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 4000);    
 }
-showPanel(0,'#3f4242');
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+var modal = document.getElementById('ticketModal');
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
